ccc433afc20c758599c682dcd9e6f83f78728aa6
Emerson-Nunes <emerson_nunes2@hotmail.com>
2026-04-20 15:07:38 -0300

    Finishing the first process, to add a simple logic .ts, html visual page and .css style

--- src/app/faq-accordion/faq-accordion.component.ts
+++ src/app/faq-accordion/faq-accordion.component.ts
@@ -1,11 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-faq-accordion',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './faq-accordion.component.html',
   styleUrl: './faq-accordion.component.scss'
 })
 export class FaqAccordionComponent {
+  openedIndex: number | null = null;
 
+  faqs = [
+    {
+      question: 'What is Angular?',
+      answer: 'Angular is a platform for building mobile and desktop web applications.',
+    },
+    {
+      question: 'What is a component in Angular?',
+      answer: 'A component controls a patch of the screen called a view. Components are the main build block of Angular applications.',
+    },
+    {
+      question: 'What are Angular directives?',
+      answer: 'Directives are instructions in the DOM. Angular directives allow you to attach behavior to elements in the DOM.',
+    },
+  ];
+
+  public toggleFAQ(index: number) {
+    this.openedIndex = this.openedIndex === index ? null : index;
+  }
 }
