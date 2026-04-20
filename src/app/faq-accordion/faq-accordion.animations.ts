1f61d5da36e4fbc98b9cad44f908b2a925cd689e
Emerson-Nunes <emerson_nunes2@hotmail.com>
2026-04-20 17:23:50 -0300

    Animation time modified to 500ms

--- src/app/faq-accordion/faq-accordion.animations.ts
+++ src/app/faq-accordion/faq-accordion.animations.ts
@@ -8,9 +8,9 @@ import {
 
 export const faqAccordionAnimations = {
   slideToggle: trigger('slideToggle', [
-    state('void', style({ heigth: 0, opacity: 0 })),
-    state('*', style({ heigth: '*', opacity: 1 })),
-    transition(':enter', [animate('200ms ease-out')]),
-    transition(':leave', [animate('100ms ease-in')]),
+    state('void', style({ height: 0, opacity: 0 })),
+    state('*', style({ height: '*', opacity: 1 })),
+    transition(':enter', [animate('500ms ease-out')]),
+    transition(':leave', [animate('500ms ease-in')]),
   ]),
 };
